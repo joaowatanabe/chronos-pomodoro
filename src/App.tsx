d4ccca558deb162f6c736096b84862b3dc75c03c
João Watanabe <jvwatanabe@gmail.com>
2026-02-14 23:37:52 -0300

    feat: useState, useEffect study, theme variation

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,14 @@ import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
 import { Footer } from './components/Footer';
-import { Heading } from './components/Heading';
-import { useState } from 'react';
+
 
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
-  //Todos componentes que usam "numero" saibam das mudanças de valor
-
-  //SEMPRE que eu usar useState, não atribuir diretamente "="
-  const [numero, setNumero] = useState(0);
-
-  const handleClick = () => {
-    setNumero(prevState => prevState + 1);
-  };
-
   return (
     <>
-      <Heading>Número: {numero}</Heading>
-      <button onClick={handleClick}>Aumenta</button>
       <Container>
         <Logo />
       </Container>
@@ -40,7 +28,7 @@ export function App() {
         <form className='form' action=''>
           <div className='formRow'>
             <DefaultInput
-              labelText={numero.toString()}
+              labelText='Task'
               placeholder='Digite uma tarefa'
               id='meuInput'
               type='text'
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,75 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import { useState, useEffect } from 'react';
 
 import styles from './styles.module.css';
 
+type AvailableThemes = 'dark' | 'light';
+
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+  const handleThemeChange = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) => {
+    event.preventDefault(); //não segue o link
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+
+    // document.documentElement.setAttribute('data-theme', theme) -> Efeito colateral, não deve ser feito, A não ser usando useEffect
+  };
+
+  // useEffect(() => {
+  //   console.log('useEffect sem dependencia', Date.now());
+  // }); // - Executado toda vez que o componente renderiza na tela.
+
+  // useEffect(() => {
+  //   console.log('useEffect com array depependencias vazio', Date.now());
+  // }, []); // - Executa apenas quando o React monta o componente na tela pela primeira vez
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+
+    // return () => {
+    //   console.log('Este componente será atualizado');
+    // }; // Clean UP
+  }, [theme]); // - Executa apensa quando o valor da dependencia theme mudar
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para a página inicial'
+        title='Ir para a página inical'
+      >
         <HouseIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para o histórico'
+        title='Ir para o histórico'
+      >
         <HistoryIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para as configurações'
+        title='Ir para as configurações'
+      >
         <SettingsIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Mudar o tema'
+        title='Mudar o tema'
+        onClick={handleThemeChange}
+      >
         <SunIcon />
       </a>
     </nav>
