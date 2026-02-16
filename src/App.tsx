63afb26c30ecc5c9df1d30f1944d584f24f7b7cf
João Watanabe <jvwatanabe@gmail.com>
2026-02-16 01:07:08 -0300

    feat: MainForm component created

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,12 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
 
+import { Footer } from './components/Footer';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { MainForm } from './components/MainForm';
 
 export function App() {
   return (
@@ -25,28 +22,7 @@ export function App() {
         <CountDown />
       </Container>
       <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              labelText='Task'
-              placeholder='Digite uma tarefa'
-              id='meuInput'
-              type='text'
-            />
-          </div>
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className='formRow'>
-            <Cycles />
-          </div>
-          <div className='formRow'>
-            <DefaultButton
-              icon={<PlayCircleIcon />}
-              color='green'
-            ></DefaultButton>
-          </div>
-        </form>
+        <MainForm />
       </Container>
       <Container>
         <Footer />
