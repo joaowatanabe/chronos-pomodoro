4c9f3f2b64a3d8befe584ef8a30d87d4d93df981
João Watanabe <jvwatanabe@gmail.com>
2026-02-16 01:59:05 -0300

    feat: GenericHtml component, AboutPomodoro and NotFound Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,8 @@
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-
-import { Footer } from './components/Footer';
+import { Home } from './pages/Home';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { MainForm } from './components/MainForm';
 
 export function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-      <Container>
-        <Menu />
-      </Container>
-      <Container>
-        <CountDown />
-      </Container>
-      <Container>
-        <MainForm />
-      </Container>
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
